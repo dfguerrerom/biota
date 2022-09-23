036354af9c48a4f9ba8b4699f590dddf9e5d3a69
dfguerrerom <dfgm2006@gmail.com>
2022-09-23 10:12:15 +0000

    build: update setup to install package data

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-import glob
 from setuptools import setup
 
-setup(name='biota',
-      packages = ['biota'],
-      version='0.3',
-      description='Tools to generate aboveground biomass forest change maps from ALOS PALSAR/PALSAR-2 mosaic data.',
-      url='https://bitbucket.org/sambowers/biota',
-      data_files=[('./cfg/',glob.glob('./cfg/*'))],
-      author='Samuel Bowers',
-      author_email='sam.bowers@ed.ac.uk',
-      license='GNU General Public License',
-      zip_safe=False)
+setup_params = {
+    "name" : 'biota',
+    "version" : '0.3',
+    "description" : 'Tools to generate aboveground biomass forest change maps from ALOS PALSAR/PALSAR-2 mosaic data.',
+    "url" : 'https://bitbucket.org/sambowers/biota',
+    "author" : 'Samuel Bowers',
+    "author_email" : 'sam.bowers@ed.ac.uk',
+    "license" : 'GNU General Public License',
+    "zip_safe" : False,
+    "packages": [
+        "biota",
+    ],
+    "package_data": {
+        "biota": [
+            "cfg/*",
+        ]
+    },
+}
+
+setup(**setup_params)
