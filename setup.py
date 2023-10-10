09c90386afef55bf594d575a5cc68b373d45740c
dfguerrerom <dfgm2006@gmail.com>
2023-10-10 12:23:52 +0200

    refactor: unpin requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup_params = {
-    "name" : 'biota',
-    "version" : '0.3',
-    "description" : 'Tools to generate aboveground biomass forest change maps from ALOS PALSAR/PALSAR-2 mosaic data.',
-    "url" : 'https://bitbucket.org/sambowers/biota',
-    "author" : 'Samuel Bowers',
-    "author_email" : 'sam.bowers@ed.ac.uk',
-    "license" : 'GNU General Public License',
-    "zip_safe" : False,
+    "name": "biota",
+    "version": "0.3",
+    "description": "Tools to generate aboveground biomass forest change maps from ALOS PALSAR/PALSAR-2 mosaic data.",
+    "url": "https://bitbucket.org/sambowers/biota",
+    "author": "Samuel Bowers",
+    "author_email": "sam.bowers@ed.ac.uk",
+    "license": "GNU General Public License",
+    "zip_safe": False,
     "packages": [
         "biota",
     ],
@@ -17,10 +17,7 @@ setup_params = {
             "cfg/*",
         ]
     },
-    "install_requires" : [
-        "scipy==1.3.3",
-        "scikit-image==0.18.3"
-    ]
+    "install_requires": ["scipy", "scikit-image"],
 }
 
 setup(**setup_params)
